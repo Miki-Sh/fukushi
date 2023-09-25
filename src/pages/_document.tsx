f22671d2f39b59d733528931a637330398b1a7ab
Miki Sh <srnin23@gmail.com>
2023-09-25 15:21:17 +0900

    📝 header部分を変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja">
+      <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+        <title>Fuku</title>
+      </Head>
       <body>
         <Main />
         <NextScript />
