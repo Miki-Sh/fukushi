40528c9767147eded7342254179c5daae9240413
Miki Sh <srnin23@gmail.com>
2023-09-28 15:18:06 +0900

    ♻️ Main.jsxをindex.jsxにまとめた

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,13 @@
-import Main from 'src/components/Main';
+import MainChoice from 'src/components/MainChoice';
 
 const App = () => {
   return (
-    <div className="text-center text-red-950">
-      <Main />
+    <div className="max-w-screen-lg mx-auto py-24 text-center text-red-950">
+      <div className = 'text-2xl'>
+        <h1 className='pb-4'>病気やけが、出産、死亡、老齢、障害、失業などで困った時は...</h1>
+        <h1>社会保障を使おう!</h1>
+      </div>
+      <MainChoice />
     </div>
   );
 };
