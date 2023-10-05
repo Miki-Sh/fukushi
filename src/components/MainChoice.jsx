85e22018db3ce20743bfcb2b753b968d9f5022ab
Miki Sh <srnin23@gmail.com>
2023-10-05 17:46:49 +0900

    💄 アイコン追加とデザイン変更

--- src/components/MainChoice.jsx
+++ src/components/MainChoice.jsx
@@ -1,21 +1,25 @@
 import Link from "next/link";
+import Image from "next/image";
 
 const MainChoice = () => {
   const ChoiceList = [
     {
       name: "養う家族がいる",
       introduction:"扶養って何？ 誰を扶養に入れられるの？ どうやって扶養に入れるの？",
-      href: "/Fuyou"
+      href: "/Fuyou",
+      img: "/baby.svg"
     },
     {
       name: "仕事を辞める",
       introduction: "退職する時に必要な手続き、もらえるお金",
-      href: "/Taisyoku"
+      href: "/Taisyoku",
+      img: "/deskwork.svg"
     },
     {
-      name: "病気になった",
-      introduction: "病気で仕事を休んだり、退職したりする時にもらえるお金、そのための手続き",
-      href: "/Syoubyou"
+      name: "病気やけがをした",
+      introduction: "けがや病気で仕事を休んだり、退職したりする時にもらえるお金、そのための手続き",
+      href: "/Syoubyou",
+      img: "/kyukyubako.svg"
     },
   ];
 
@@ -23,8 +27,11 @@ const MainChoice = () => {
     <div className="my-12 flex flex-wrap justify-center items-center">
       {ChoiceList.map((items) => (
       <Link href={items.href} key={items.name}>
-        <div className='inline-block h-60 w-60 mr-8 mb-8 text-center rounded-3xl bg-gradient-to-t from-emerald-100 via-emerald-50 to-teal-100 shadow-2xl cursor-pointer'>
-          <h1 className='pt-12 pb-8 px-2 text-3xl'>{items.name}</h1>
+        <div className='inline-block h-64 w-64 mr-8 mb-8 text-center rounded-3xl bg-gradient-to-t from-emerald-100 via-emerald-50 to-teal-100 shadow-2xl cursor-pointer'>
+          <h1 className='pt-12 px-2 text-3xl'>{items.name}</h1>
+          <div className="py-2 flex justify-center">
+          <Image src={items.img} alt="choice" height={50} width={50} />
+          </div>
           <p className='pb-6 pl-3 pr-2 text-left text-sky-800 text-lg'>{items.introduction}</p>
         </div>
       </Link>
--- src/pages/Fuyou.jsx
+++ src/pages/Fuyou.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Image from "next/image";
 import ZeiFuyou from "src/components/ZeiFuyou";
 import KenpoFuyou from "src/components/KenpoFuyou";
 import NenFuyou from "src/components/NenFuyou";
@@ -21,7 +22,7 @@ const Fuyou = () => {
       <div className="my-12 flex flex-wrap justify-center items-center">
           
         <div className='inline-block lg:w-2/5 mx-8 mb-8 p-8 text-center rounded-3xl border-4 border-teal-500'>
-          <h1 className='pt-12 pb-8 px-2 text-3xl'>税金の扶養</h1>
+          <h1 className='pt-12 pb-8 px-2 text-3xl text-red-950'>税金の扶養</h1>
           <p className='pb-6 pl-3 pr-2 text-left text-sky-800 text-lg'>
             　税金に関する扶養では、金銭面で支えている人を扶養に入れると、支えている人が払う税金（所得税や住民税）が安くなります。<br />
             　例えば、会社員の人が、パートで働く奥さんがいたり、田舎に暮らす両親に仕送りをして支えている場合、年末調整で扶養に入れる手続きをすると、所得税や住民税が安くなります。</p>
@@ -38,7 +39,7 @@ const Fuyou = () => {
             /> }
         </div>
         <div className='inline-block lg:w-2/5 mx-8 mb-8 p-8 text-center rounded-3xl border-4 border-teal-500'>
-          <h1 className='pt-12 pb-8 px-2 text-3xl'>年金の扶養</h1>
+          <h1 className='pt-12 pb-8 px-2 text-3xl text-red-950'>年金の扶養</h1>
           <p className='pb-6 pl-3 pr-2 text-left text-sky-800 text-lg'>
             　年金の扶養に入れると、扶養されている人が自分で保険料を払わなくて良くなります。<br />
             　例えば、厚生年金に加入している人が、パートナー（事実婚を含む）を扶養に入れると、パートナーは保険料を自分で払わなくて良くなります。</p>
@@ -57,15 +58,15 @@ const Fuyou = () => {
             /> }
         </div>
         <div className='inline-block lg:w-2/5 mx-8 mb-8 p-8 text-center rounded-3xl border-4 border-teal-500'>
-          <h1 className='pt-12 pb-8 px-2 text-3xl'>健康保険の扶養</h1>
+          <h1 className='pt-12 pb-8 px-2 text-3xl text-red-950'>健康保険の扶養</h1>
           <p className='pb-6 pl-3 pr-2 text-left text-sky-800 text-lg'>
             　健康保険の扶養に入れると、扶養されている人が自分で健康保険料を払わなくて良くなります。<br />
             　例えば、会社の健康保険組合に加入している人が、子どもや、仕送りで支えている両親を扶養に入れると、子どもや両親は健康保険料を自分で払わなくて良くなります。</p>
           <p className='pb-6 pl-3 pr-2 text-left text-sky-800 text-lg'>
             　扶養に入れられる条件は、加入している健康保険組合によって様々です。詳しくは、自分が加入している健康保険組合に確認してみてください。
             今回は、一番代表的な「全国健康保険協会（協会けんぽ）」の場合で診断してみましょう。（他の健康保険組合に加入している方も参考までに診断してみてください）</p>
-          <p className='pb-6 pl-3 pr-2 text-left text-red-800 text-lg'>
-            ※自営業の人などが加入している国民健康保険には、そもそも扶養が無いので家族を扶養に入れることができません。</p>
+          <div className="flex items-center"><p className='pb-6 pl-3 pr-2 text-left text-red-800 text-lg'>
+          <Image src='/!.svg' alt="!" height={20} width={20} />自営業の人などが加入している国民健康保険には、そもそも扶養が無いので家族を扶養に入れることができません。</p></div>
           <button
             className="py-3 px-6 text-center shadow-md rounded-full text-xl font-semibold 
               bg-teal-500 hover:bg-white text-white hover:text-teal-500 border-4 hover:border-4 border-teal-500
