e253fc7083a605e630f43bec1004c7d0f76f0c2e
Miki Sh <srnin23@gmail.com>
2023-10-06 17:08:57 +0900

    💄 デザイン修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,11 +5,12 @@ import Header from 'src/components/Header';
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <><Head>
+    <>
+    <Head>
       <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta name="theme-color" content="#000000" />
-      <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+      <link rel="icon" href="/favicon.ico" />
       <title>Fuku</title>
     </Head>
     <Header />
