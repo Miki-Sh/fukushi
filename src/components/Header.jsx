81fee3f2825086191597ce54169eb3002655245d
Miki Sh <srnin23@gmail.com>
2023-10-13 01:51:55 +0900

    ♻️ リファクタリング

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import Image from "next/image";
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 const Header = () => {
   return (
-    <div className="sticky top-0 h-12 w-full flex items-center pl-2 bg-amber-50 border-dashed border-b-8 border-orange-200 text-red-950 ">
-      <Link href="/" className="flex items-center hover:text-orange-500">
-        <Image src="/HOME.svg" alt="HOME" height={30} width={30} className="mr-2 hover:fill-orange-500" />
+    <div className='sticky top-0 h-12 w-full flex items-center pl-2 bg-amber-50 border-dashed border-b-8 border-orange-200 text-red-950 '>
+      <Link href='/' className='flex items-center hover:text-orange-500'>
+        <Image src='/HOME.svg' alt='HOME' height={30} width={30} className='mr-2 hover:fill-orange-500' />
         最初のページに戻る
       </Link>
     </div>
--- src/pages/Syoubyou.jsx
+++ src/pages/Syoubyou.jsx
@@ -1,7 +1,11 @@
 const Syoubyou = () => {
   return (
-    <div className="text-center">
-      <h1>傷病手当金って？</h1>
+    <div className='my-12 mx-12 text-red-950 text-xl'>
+      <h1 className='pb-8 text-center text-indigo-900 text-3xl'>傷病手当金って？</h1>
+      <p className=''>
+
+      </p>
+
     </div>
   );
 };
