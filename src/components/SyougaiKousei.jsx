e7cef245dd7d7cdbc9275e57f8606d04cacf820d
Miki Sh <srnin23@gmail.com>
2023-10-24 14:58:19 +0900

    🐛 初期表示とモーダル閉じるの修正

--- src/components/SyougaiKousei.jsx
+++ src/components/SyougaiKousei.jsx
@@ -3,7 +3,7 @@ import Modal from 'src/components/Modal';
 
 const SyougaiKousei = (props) => {
   const setKouseiOpen = props.setKouseiOpen;
-  const [ i, setI ] = useState(2);
+  const [ i, setI ] = useState(4);
   const list = [
     {
       'q': '障害厚生年金の受給要件に当てはまります！',
@@ -64,17 +64,18 @@ const SyougaiKousei = (props) => {
         <h1 className='mt-10 mb-4 text-2xl'>{list[i].q}</h1>
         {!(i===0||i===1||i===2||i===3) && ( 
           <div>
-            <button className=' py-3 px-11 text-center shadow-md rounded-lg text-xl font-semibold 
+            <button className='py-3 px-10 text-center shadow-md rounded-lg text-xl font-semibold 
               bg-pink-600 hover:bg-white text-white hover:text-pink-600 border-4 hover:border-4 border-pink-600
               transition duration-200 ease-in'
               onClick={() => setI(list[i].yes)}>はい</button>
-            <button className='ml-6 py-3 px-10 text-center shadow-md rounded-lg text-xl font-semibold 
+            <button className='ml-6 py-3 px-9 text-center shadow-md rounded-lg text-xl font-semibold 
               bg-pink-600 hover:bg-white text-white hover:text-pink-600 border-4 hover:border-4 border-pink-600
               transition duration-200 ease-in'
               onClick={() => setI(list[i].no)}>いいえ</button>
           </div>
         )}
       </> }
+      setModalOpen = { setKouseiOpen }
     />
   );
 };
